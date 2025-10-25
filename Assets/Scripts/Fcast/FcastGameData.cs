24faafd4a91d181ce5b01b0140be01d6c6f7a5c3
Korey Hinton <2801399+koreyhinton@users.noreply.github.com>
2025-10-25 06:25:39 -0400

    make buildings not walkable once built

--- Assets/Scripts/Fcast/FcastGameData.cs
+++ Assets/Scripts/Fcast/FcastGameData.cs
@@ -36,12 +36,10 @@ namespace Fcast
         public BuildingRazer BuildingRazer { get; set; } // input (set null to remove it)
         public BuildingSpawner QueuedSpawner { get; set; } // output (for new spawners)
         public BuildingRazer QueuedRazer { get; set; } // output (for new razers)
-        //public BuildingEventIntervalCheck BuildingEventIntervalCheck { get; set; } = new BuildingEventIntervalCheck();
-        //public BuildingUpdateViewsCheck BuildingUpdateViewsCheck { get; set; } = new BuildingUpdateViewsCheck();
         public BuildingPreviewUpdater BuildingPreviewUpdater { get; set; } = new BuildingPreviewUpdater();
         public InputBuildSequenceCheck InputBuildSequenceCheck { get; set; } = new InputBuildSequenceCheck();
         public DiffLog DiffLog { get; set; } = new DiffLog();
-        public IGrid Grid { get; set; }
+        public Osnowa.Osnowa.Grid.Grid Grid { get; set; }
         public bool Frame { get; set; }
     }
 }
